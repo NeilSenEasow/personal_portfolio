22ee30a1b8c28e3e4766ff1f56f3d4d5c5167ea3
Neil Sen Easow <neilseneasow@gmail.com>
2024-12-06 06:25:07 +0530

    Add navbar social icons

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,21 +1,29 @@
 import React from "react"
-// import ReactDOM from "react-dom"
 import { Container, Navbar } from 'react-bootstrap';
 import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function NavBar()   {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Brand href="#home">NeilSenEasow</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav">
+                    <span className="navbar-toggler-icon"></span>
+                </Navbar.Toggle>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#skills">Skills</Nav.Link>
-                    <Nav.Link href="#projects">Projects</Nav.Link>
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#skills">Skills</Nav.Link>
+                        <Nav.Link href="#projects">Projects</Nav.Link>
                     </Nav>
+                    <span className="navbar-text">
+                        <div className="social-icon">
+                            <a href="https://www.instagram.com/"><i class="bi bi-instagram"></i></a>
+                            <a href="https://web.whatsapp.com/"><i class="bi bi-whatsapp"></i></a>
+                            <a href="https://twitter.com/"><i class="bi bi-twitter-x"></i></a>
+                        </div>
+                    </span>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
