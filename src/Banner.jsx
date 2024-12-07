63d6b4084cab2457583268150e620c71b897ca37
Neil Sen Easow <neilseneasow@gmail.com>
2024-12-07 18:13:39 +0530

    Add banner content

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-function Banner()   {
-    return (
-        <div className="banner-content">
-            Hello There!
-        </div>
-    );
+function Banner() {
+  const texts = ["Hi There!", "I'm Neil", "I'm a front-end developer"];
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % texts.length);
+    }, 2000); 
+
+    return () => clearInterval(interval);
+  }, [texts.length]);
+
+  return (
+    <div className="banner">
+      <p>{texts[currentIndex]}</p>
+    </div>
+  );
 }
 
 export default Banner;
