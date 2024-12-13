e0c646b91d2fab04de2fbca2ccb06daec08a410b
Neil Sen Easow <neilseneasow@gmail.com>
2024-12-13 23:27:27 +0530

    Modify text carousel

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -5,6 +5,50 @@ import { faCode, faBrain, faUsers } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import "./Projects.css";
 
+const viewProjectLinkStyles = {
+  display: 'inline-block',
+  padding: '10px 20px',
+  backgroundColor: '#121212',
+  color: '#fff',
+  textDecoration: 'none',
+  borderRadius: '5px',
+  transition: 'background-color 0.3s ease',
+  border: '1px solid #fff',
+  cursor: 'pointer', 
+};
+
+const projects = [
+    {
+        title: 'Keeper App',
+        description: 'A note-taking app to keep your daily tasks and notes organized.',
+        image: require('./assets/images/KeeperApp.png'),
+        alt: 'Keeper App',
+        link: '#', 
+      },
+      {
+        title: 'To Do List',
+        description: 'A simple application to track your pending tasks and achieve your goals.',
+        image: require('./assets/images/ToDoList.png'),
+        alt: 'To Do List',
+        link: '#', 
+      },
+      {
+        title: 'Genesis',
+        description: 'A website to showcase projects using different libraries and technologies.',
+        image: require('./assets/images/Genesis.png'),
+        alt: 'Genesis',
+        link: '#', 
+      },
+      {
+        title: 'Career Finder',
+        description: 'A website to find possible careers based on your skills and experience.',
+        image: require('./assets/images/CareerFinder.png'),
+        alt: 'Career Finder',
+        link: '#', 
+      },
+];
+
+
 function Projects() {
     return (
         <section className="skill">
@@ -13,97 +57,31 @@ function Projects() {
                 <p>Here are some of my projects, showcasing my skills and expertise in different technologies.</p>
                 <div className="skill-slider">
                     <Carousel>
-                        <Carousel.Item interval={1000}>
-                            <img
-                                className="carousel-image"
-                                src={require('./assets/images/KeeperApp.png')}
-                                alt="Keeper App"
-                            />
-                            <Carousel.Caption className="carousel-caption-custom">
-                                 <h3 data-text="Keeper App">Keeper App</h3>
-                                 <p data-text="A note-taking app to keep your daily tasks and notes organized.">A note-taking app to keep your daily tasks and notes organized.</p>
-                            </Carousel.Caption>
-                        </Carousel.Item>
-                        <Carousel.Item interval={500}>
-                            <img
-                                className="carousel-image"
-                                src={require('./assets/images/ToDoList.png')}
-                                alt="To Do List"
-                            />
-                            <Carousel.Caption className="carousel-caption-custom">
-                                 <h3 data-text="To Do List">To Do List</h3>
-                                <p data-text="A simple application to track your pending tasks and achieve your goals.">A simple application to track your pending tasks and achieve your goals.</p>
-                            </Carousel.Caption>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img
-                                className="carousel-image"
-                                src={require('./assets/images/Genesis.png')}
-                                alt="Genesis"
-                            />
-                            <Carousel.Caption className="carousel-caption-custom">
-                                <h3 data-text="Genesis">Genesis</h3>
-                                <p data-text="A website to showcase projects using different libraries and technologies.">A website to showcase projects using different libraries and technologies.</p>
-                            </Carousel.Caption>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img
-                                className="carousel-image"
-                                src={require('./assets/images/CareerFinder.png')}
-                                alt="Career Finder"
-                            />
-                            <Carousel.Caption className="carousel-caption-custom">
-                                <h3 data-text="Career Finder">Career Finder</h3>
-                                 <p data-text="A website to find possible careers based on your skills and experience.">A website to find possible careers based on your skills and experience.</p>
-                            </Carousel.Caption>
-                        </Carousel.Item>
+                        {projects.map((project, index) => (
+                          <Carousel.Item key={index} interval={1000}>
+                                <img
+                                    className="carousel-image"
+                                    src={project.image}
+                                    alt={project.alt}
+                                />
+                                <Carousel.Caption className="carousel-caption-custom">
+                                    <h3 data-text={project.title}>{project.title}</h3>
+                                    <p data-text={project.description}>{project.description}</p>
+                                     <div style={{ marginTop: '20px', textAlign: 'center' }}>
+                                         <a
+                                              className="view-project"
+                                              href={project.link}
+                                              style={viewProjectLinkStyles}
+                                              onMouseOver={e => {e.target.style.backgroundColor = '#fff'; e.target.style.color = '#121212';}}
+                                              onMouseOut={e => {e.target.style.backgroundColor = '#121212'; e.target.style.color = '#fff';}}
+                                          >
+                                              View Project
+                                          </a>
+                                      </div>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                         ))}
                     </Carousel>
-                </div>
-            </div>
-             <div className="skill-bx">
-                <div className="skills-grid">
-                     {/* Technical Skills */}
-                     <div className="skill-category">
-                         <h3>Technical Skills</h3>
-                        <div className="skill-list">
-                            <div className="skill-item">
-                                <FontAwesomeIcon icon={faReact} size="2xl"/>
-                                <span>React</span>
-                            </div>
-                            <div className="skill-item">
-                                <FontAwesomeIcon icon={faJs} size="2xl"/>
-                                <span>JavaScript</span>
-                            </div>
-                            <div className="skill-item">
-                                <FontAwesomeIcon icon={faPython} size="2xl"/>
-                                <span>Python</span>
-                            </div>
-                             <div className="skill-item">
-                                <FontAwesomeIcon icon={faNodeJs} size="2xl"/>
-                                <span>Node.js</span>
-                            </div>
-                             <div className="skill-item">
-                                <FontAwesomeIcon icon={faCode} size="2xl"/>
-                                <span>HTML/CSS</span>
-                            </div>
-                        </div>
-                    </div>
-
-                    {/* Soft Skills */}
-                     <div className="skill-category">
-                        <h3>Soft Skills</h3>
-                         <div className="skill-list">
-                            <div className="skill-item">
-                                <FontAwesomeIcon icon={faBrain} size="2xl"/>
-                                <span>Problem-solving</span>
-                            </div>
-                             <div className="skill-item">
-                                <FontAwesomeIcon icon={faUsers} size="2xl"/>
-                                <span>Teamwork</span>
-                            </div>
-                        </div>
-                    </div>
-
                 </div>
             </div>
         </section>
