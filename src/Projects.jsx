5b459872668d166037fdff88fbc5c2a070a14b54
Neil Sen Easow <neilseneasow@gmail.com>
2024-12-13 19:32:34 +0530

    Add changes to projects section

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,15 +1,58 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Carousel from 'react-bootstrap/Carousel';
 import { faReact, faJs, faPython, faNodeJs } from '@fortawesome/free-brands-svg-icons';
 import { faCode, faBrain, faUsers } from '@fortawesome/free-solid-svg-icons';
-import './Skills.css'; // Import the custom CSS file
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import './Projects.css'; Import the custom CSS file
 
-function Skills() {
+function Projects() {
     return (
         <section className="skill">
-            <div className="skill-bx">
+          <div className="skill-bx">
                 <h2>Skills</h2>
-                <p>My technical and soft skills.</p>
+                <p>Some of my skills and expertise in different technologies.</p>
+
+                <div className="skill-slider">
+                    <Carousel>
+                        <Carousel.Item interval={1000}>
+                            <img
+                                className="carousel-image"
+                                src={require('./assets/images/image1.png')}
+                                alt="First slide"
+                            />
+                            <Carousel.Caption>
+                                <h3>First slide label</h3>
+                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item interval={500}>
+                            <img
+                                className="carousel-image"
+                                src={require('./assets/images/image2.png')}
+                                alt="Second slide"
+                            />
+                            <Carousel.Caption>
+                                <h3>Second slide label</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="carousel-image"
+                                src={require('./assets/images/image3.png')}
+                                alt="Third slide"
+                            />
+                            <Carousel.Caption>
+                                <h3>Third slide label</h3>
+                                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    </Carousel>
+                </div>
+            </div>
+             <div className="skill-bx">
+                {/* <h2>Skills</h2>
+                <p>My technical and soft skills.</p> */}
                 <div className="skills-grid">
                      {/* Technical Skills */}
                      <div className="skill-category">
@@ -59,4 +102,4 @@ function Skills() {
     );
 }
 
-export default Skills;
+export default Projects;
